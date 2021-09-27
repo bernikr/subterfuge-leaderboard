4a89f2d135be65a5ffdd2d93f4278d0c4b2c8dd0
bernikr <berni98k@gmail.com>
2021-09-27 14:49:07 +0200

    add str methods to the models

--- leaderboard/models.py
+++ leaderboard/models.py
@@ -5,6 +5,9 @@ class Player(models.Model):
     name = models.TextField(null=False, blank=False, unique=True)
     joined = models.DateField(null=False)
 
+    def __str__(self):
+        return self.name
+
 
 class LeaderboardEntry(models.Model):
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
@@ -27,3 +30,6 @@ class LeaderboardEntry(models.Model):
             models.UniqueConstraint(fields=['player', 'timestamp'], name='single player at time'),
             models.UniqueConstraint(fields=['rank', 'timestamp'], name='single rank at time'),
         ]
+
+    def __str__(self):
+        return f"{self.rank}. {self.player.name} on {self.timestamp}"
