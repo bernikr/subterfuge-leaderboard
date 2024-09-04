505048f0fc49a659eede61f25a26b2410d267a98
bernikr <git@berni.kr>
2024-09-04 12:47:46 +0200

    delete django site

--- dataserver/website/leaderboard/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from leaderboard.models import Player, LeaderboardEntry, Leaderboard
-
-admin.site.register(Player)
-admin.site.register(Leaderboard)
-admin.site.register(LeaderboardEntry)
